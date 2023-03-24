4a520e067be28dbd8f3272779da8d6a1918ce27f
Nnamani John <nnamanijohn123@gmail.com>
2023-03-24 14:56:30 -0700

    fixes

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # creates a file in /tmp
 
-file { '/tmp/school':
-  content =>'I love Puppet',
+file { 'school':
+  path    => '/tmp/school',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
